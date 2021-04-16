4922c74a00ebd809babdde944260caf6b3ca19b7
GMITNathan <49440805+GMITNathan@users.noreply.github.com>
2021-04-16 15:04:36 +0100

    login-register page added
    Access to views restricted unless logged in. Moved all pages to view. A redirect index.html file is in the public. This file simply redirects if index.html is called. Login uses MySQL and NodeJS

---
+++ database.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`name`, `email`, `password`)
+VALUES
+	('admin', 'admin@test.com', 'password'),
+	('username', 'username@test.com', 'password');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
--- server.js
+++ server.js
@@ -4,27 +4,234 @@ if (process.env.NODE_ENV !== 'production') {
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
 const port = process.env.port || 3000
 
 console.log(stripeSecretKey, stripePublicKey)
 
-const express = require('express')
-const app = express()
+
 const fs = require('fs')
 const stripe = require('stripe')(stripeSecretKey)
 
-app.set('view engine', 'ejs')
-app.use(express.json())
-app.use(express.static('public'))
+const express = require('express');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcryptjs');
+const dbConnection = require('./database');
+const { body, validationResult } = require('express-validator');
 
-app.get('/', (req, res) => {
-    res.sendFile('./public/index.html', {root: __dirname})
+const app = express();
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.json())
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public'), { index:false }));
+
+// SET OUR VIEWS AND VIEW ENGINE
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+// APPLY COOKIE SESSION MIDDLEWARE
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2'],
+    maxAge:  3600 * 1000 // 1hr
+}));
+
+// DECLARING CUSTOM MIDDLEWARE
+const ifNotLoggedin = (req, res, next) => {
+    if(!req.session.isLoggedIn){
+        return res.render('login-register');
+    }
+    next();
+}
+
+const ifLoggedin = (req,res,next) => {
+    if(req.session.isLoggedIn){
+        return res.redirect('/index');
+    }
+    next();
+}
+// END OF CUSTOM MIDDLEWARE
+
+// ROOT PAGE
+app.get('/', ifNotLoggedin, (req,res,next) => {
+    dbConnection.execute("SELECT `name` FROM `users` WHERE `id`=?",[req.session.userID])
+    .then(([rows]) => {
+        res.render('index',{
+            name:rows[0].name
+        });
+    });
+    
+});// END OF ROOT PAGE
+
+app.get('/index', (req, res) => {
+    if(!req.session.isLoggedIn){
+        return res.redirect('/');
+    } else {
+        return res.render('index')
+    }
 })
+
+app.get('/portfolio', (req, res) => {
+    if(!req.session.isLoggedIn){
+        return res.redirect('/');
+    } else {
+        return res.render('portfolio')
+    }
+})
+
+app.get('/pieces/low-poly-animation', (req, res) => {
+    if(!req.session.isLoggedIn){
+        return res.redirect('/');
+    } else {
+        return res.render('pieces/low-poly-animation')
+    }
+})
+
+app.get('/pieces/cityscape-animation', (req, res) => {
+    if(!req.session.isLoggedIn){
+        return res.redirect('/');
+    } else {
+        return res.render('pieces/cityscape-animation')
+    }
+})
+
+
+
+// REGISTER PAGE
+app.post('/register', ifLoggedin, 
+// post data validation(using express-validator)
+[
+    body('user_email','Invalid email address!').isEmail().custom((value) => {
+        return dbConnection.execute('SELECT `email` FROM `users` WHERE `email`=?', [value])
+        .then(([rows]) => {
+            if(rows.length > 0){
+                return Promise.reject('This E-mail already in use!');
+            }
+            return true;
+        });
+    }),
+    body('user_name','Username is Empty!').trim().not().isEmpty(),
+    body('user_pass','The password must be of minimum length 6 characters').trim().isLength({ min: 6 }),
+],// end of post data validation
+(req,res,next) => {
+
+    const validation_result = validationResult(req);
+    const {user_name, user_pass, user_email} = req.body;
+    // IF validation_result HAS NO ERROR
+    if(validation_result.isEmpty()){
+        // password encryption (using bcryptjs)
+        bcrypt.hash(user_pass, 12).then((hash_pass) => {
+            // INSERTING USER INTO DATABASE
+            dbConnection.execute("INSERT INTO `users`(`name`,`email`,`password`) VALUES(?,?,?)",[user_name,user_email, hash_pass])
+            .then(result => {
+                res.send(`your account has been created successfully, Now you can <a href="/">Login</a>`);
+            }).catch(err => {
+                // THROW INSERTING USER ERROR'S
+                if (err) throw err;
+            });
+        })
+        .catch(err => {
+            // THROW HASING ERROR'S
+            if (err) throw err;
+        })
+    }
+    else{
+        // COLLECT ALL THE VALIDATION ERRORS
+        let allErrors = validation_result.errors.map((error) => {
+            return error.msg;
+        });
+        // REDERING login-register PAGE WITH VALIDATION ERRORS
+        res.render('login-register',{
+            register_error:allErrors,
+            old_data:req.body
+        });
+    }
+});// END OF REGISTER PAGE
+
+app.get('/register', (req, res) => {
+    if(!req.session.isLoggedIn){
+        return res.redirect('/');
+    } else {
+        return res.render('index')
+    }
+})
+
+// LOGIN PAGE
+app.post('/', ifLoggedin, [
+    body('user_email').custom((value) => {
+        return dbConnection.execute('SELECT `email` FROM `users` WHERE `email`=?', [value])
+        .then(([rows]) => {
+            if(rows.length == 1){
+                return true;
+                
+            }
+            return Promise.reject('Invalid Email Address!');
+            
+        });
+    }),
+    body('user_pass','Password is empty!').trim().not().isEmpty(),
+], (req, res) => {
+    const validation_result = validationResult(req);
+    const {user_pass, user_email} = req.body;
+    if(validation_result.isEmpty()){
+        
+        dbConnection.execute("SELECT * FROM `users` WHERE `email`=?",[user_email])
+        .then(([rows]) => {
+            bcrypt.compare(user_pass, rows[0].password).then(compare_result => {
+                if(compare_result === true){
+                    req.session.isLoggedIn = true;
+                    req.session.userID = rows[0].id;
+
+                    res.redirect('/');
+                }
+                else{
+                    res.render('login-register',{
+                        login_errors:['Invalid Password!']
+                    });
+                }
+            })
+            .catch(err => {
+                if (err) throw err;
+            });
+
+
+        }).catch(err => {
+            if (err) throw err;
+        });
+    }
+    else{
+        let allErrors = validation_result.errors.map((error) => {
+            return error.msg;
+        });
+        // REDERING login-register PAGE WITH LOGIN VALIDATION ERRORS
+        res.render('login-register',{
+            login_errors:allErrors
+        });
+    }
+});
+// END OF LOGIN PAGE
+
+// LOGOUT
+app.get('/logout',(req,res)=>{
+    //session destroy
+    req.session = null;
+    res.redirect('/');
+});
+// END OF LOGOUT
+
+
+
+
+
 
 app.get('/shop', function (req, res) {
     fs.readFile('items.json', function (error, data) {
         if (error) {
             res.status(500).end()
+        } else if (!req.session.isLoggedIn){
+            return res.redirect('/');
         } else {
             res.render('shop', {
                 stripePublicKey: stripePublicKey,
@@ -65,3 +272,5 @@ app.post('/purchase', function (req, res) {
 })
 
 app.listen(port, () => console.log('Server running on port: '+port))
+
+//app.listen(3000, () => console.log("Server is Running..."));
